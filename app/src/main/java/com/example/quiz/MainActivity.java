c29ddd4e0d0d119c5f18962fb7115b82d8b7894f
Nil <citis@citis.ru>
2023-02-07 17:12:34 +0300

    Lesson 6 create 2 page - GameLevels with 5 level buttons

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(getApplicationContext(), GameLevels.class);
+                    startActivity(intent); finish();
+                }catch (Exception e){
+
+                }
+               }
+           }
+        );
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
