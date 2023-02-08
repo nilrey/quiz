016414c4278774f0a9040e89c3cf0108ec7707e3
Nil <citis@citis.ru>
2023-02-08 14:17:44 +0300

    Lesson 7 back button in GameLevel page

--- app/src/main/java/com/example/quiz/GameLevels.java
+++ app/src/main/java/com/example/quiz/GameLevels.java
@@ -1,9 +1,12 @@
 package com.example.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class GameLevels extends AppCompatActivity {
 
@@ -14,5 +17,32 @@ public class GameLevels extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+
+        button_back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent); finish();
+                    }catch (Exception e){
+
+                    }
+                }
+            }
+        );
     }
+    //Системная кнопка "Назад" - начало
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent); finish();
+        }catch (Exception e){
+
+        }
+    }
+    //Системная кнопка "Назад" - конец
 }
